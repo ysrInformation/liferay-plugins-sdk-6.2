ce260ebd7585c0b5341dad6ce55cc5beca6ee6a0
yasin10414 <yasin10414@gmail.com>
2015-08-09 00:26:57 +0530

    added constants

--- portlets/htmsd-portlet/docroot/WEB-INF/src/com/htmsd/util/HConstants.java
+++ portlets/htmsd-portlet/docroot/WEB-INF/src/com/htmsd/util/HConstants.java
@@ -2,19 +2,26 @@ package com.htmsd.util;
 
 public interface HConstants {
 
+	public static String ITEM_ID = "itemId";
 	public static String NAME = "name";
 	public static String DESCRIPTION = "description";
 	public static String PRICE = "price";
+	public static String TOTAL_PRICE="totalPrice";
 	public static String CATEGORY = "category";
 	public static String TAG = "tag";
 	public static String CATEGORY_ID = "categoryId";
 	public static String TAG_ID = "tagId";
 	public static String IMAGE = "image";
+	public static String IMAGE_ID = "imageId";
+	public static String IMAGE_UPLOAD_PREVIEW = "image_upload_preview";
+	public static String DELETE_IMAGE = "deleteImage";
+	public static String status = "status";
 	public static String ITEM_FOLDER_NAME = "Items Added";
 	public static int IMAGES_UPLOAD_LIMIT = 5;
 	public static int NEW = 0;
 	public static int APPROVE = 1;
 	public static int REJECT = 5;
 	public static int PENDING = 2;
+	
 
 }
