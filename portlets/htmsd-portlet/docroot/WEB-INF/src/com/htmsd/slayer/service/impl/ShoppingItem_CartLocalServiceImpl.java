4a087f7107fc043764c44f9713fc4025f63429b2
riyazpasha <riyaz.pasha66@yahoo.com>
2015-08-15 17:55:14 +0530

    Committing LocalServiceImpl

---
+++ portlets/htmsd-portlet/docroot/WEB-INF/src/com/htmsd/slayer/service/impl/ShoppingItem_CartLocalServiceImpl.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.htmsd.slayer.service.impl;
+
+import com.htmsd.slayer.service.base.ShoppingItem_CartLocalServiceBaseImpl;
+
+/**
+ * The implementation of the shopping item_ cart local service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.htmsd.slayer.service.ShoppingItem_CartLocalService} interface.
+ *
+ * <p>
+ * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * </p>
+ *
+ * @author YSR
+ * @see com.htmsd.slayer.service.base.ShoppingItem_CartLocalServiceBaseImpl
+ * @see com.htmsd.slayer.service.ShoppingItem_CartLocalServiceUtil
+ */
+public class ShoppingItem_CartLocalServiceImpl
+	extends ShoppingItem_CartLocalServiceBaseImpl {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this interface directly. Always use {@link com.htmsd.slayer.service.ShoppingItem_CartLocalServiceUtil} to access the shopping item_ cart local service.
+	 */
+}
