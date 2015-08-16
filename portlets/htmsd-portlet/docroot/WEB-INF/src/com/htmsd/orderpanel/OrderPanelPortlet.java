236ba3469d97e6f6a135362cb34f78577e2f6e45
riyazpasha <riyaz.pasha66@yahoo.com>
2015-08-17 00:36:59 +0530

    commiting orderpanel actionclass

--- portlets/htmsd-portlet/docroot/WEB-INF/src/com/htmsd/orderpanel/OrderPanelPortlet.java
+++ portlets/htmsd-portlet/docroot/WEB-INF/src/com/htmsd/orderpanel/OrderPanelPortlet.java
@@ -1,11 +1,33 @@
 package com.htmsd.orderpanel;
 
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+
+import com.htmsd.slayer.service.ShoppingOrderLocalServiceUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 /**
  * Portlet implementation class OrderPanelPortlet
  */
 public class OrderPanelPortlet extends MVCPortlet {
- 
+	
+	public void updateOrderStatus(ActionRequest actionRequest,
+			ActionResponse actionResponse) throws IOException, PortletException {
+		
+		_log.info("In updateOrderStatus ..."); 
+		int orderStatus = ParamUtil.getInteger(actionRequest, "orderStatus");
+		long orderId = ParamUtil.getLong(actionRequest, "orderId");
+		
+		ShoppingOrderLocalServiceUtil.updateShoppingOrder(orderStatus, orderId);
+		
+	}
+
+	private final Log _log = LogFactoryUtil.getLog(OrderPanelPortlet.class);
 
 }
