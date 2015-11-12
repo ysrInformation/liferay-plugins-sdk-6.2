888c8ce6ac3a13ed25ad06acb994ccece4e7a176
riyazpasha <riyaz.pasha66@yahoo.com>
2015-11-12 21:23:37 +0530

    Added an Entry in contants

--- portlets/htmsd-portlet/docroot/WEB-INF/src/com/htmsd/util/HConstants.java
+++ portlets/htmsd-portlet/docroot/WEB-INF/src/com/htmsd/util/HConstants.java
@@ -80,6 +80,7 @@ public interface HConstants {
 	public static String SHIPPING_STATUS = "Shipping";
 	public static String PROCESSING_STATUS = "Processing";
 	public static String ORDER_CANCELLED_STATUS = "Order Cancelled";
+	public static String CANCEL_ORDER_STATUS = "Cancel Order";
 
 	public static DateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
 
