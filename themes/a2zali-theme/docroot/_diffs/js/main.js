63625aa40ef5c80916721a9a0ede6b67377a3ced
saleem786khan <786khan1saleem@gmail.com>
2016-09-01 23:41:17 +0530

    Updated code for scroll and added js to main.js and updated the carousel.vm template

--- themes/a2zali-theme/docroot/_diffs/js/main.js
+++ themes/a2zali-theme/docroot/_diffs/js/main.js
@@ -26,3 +26,64 @@ $(function() {
 	$(".quick-contact .control-group").removeClass("lfr-textarea-container");
 	$(".quick-contact .control-group").addClass( "form-inline" );
 });
+
+
+//Search 
+var elTop = $('#search-div').offset().top;
+var mobile = !(Liferay.Util.isPhone() || Liferay.Util.isTablet());
+	$(window).scroll(function() {
+		if(mobile) {
+        $('#search-div').toggleClass('sticky', $(window).scrollTop() > elTop);
+        $('#fixed_div').toggleClass('fixed_div', $(window).scrollTop() > elTop);
+	} else {
+		$('#srch-btn').html('<i class="fa fa-search"></i>');
+	}
+});
+
+//Scroll to Top
+$(function(){
+	$(document).on( 'scroll', function(){
+		if ($(window).scrollTop() > 100) {
+			$('#to_top').addClass('disabled');
+		} else {
+			$('#to_top').removeClass('disabled');
+		}
+	});
+	$('#to_top').on('click', scrollToTop);
+	
+	// auto complete code
+	AUI().use('aui-base', function(A) {
+		Liferay.Service('/htmsd-portlet.shoppingitem/get-autocomplete-items',
+	  		function(data) {
+	  			var autocompleteData = data.autoCompleteData;
+	  			console.log(autocompleteData);
+				$("#search").autocomplete({
+					source : autocompleteData
+				});
+	  		}
+	  	);
+	});
+	
+	function scrollToTop() {
+		verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+		element = $('body');
+		offset = element.offset();
+		offsetTop = offset.top;
+		$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+	}
+
+	AUI().use('aui-base', function(A) {
+		Liferay.Service('/htmsd-portlet.shoppingcart/get-shopping-cart-item-count', {
+			userId: $user_id
+		}, function(obj) {
+			$('#cart-item-count').html("&nbsp;"+obj+"&nbsp;");
+		});
+	});
+
+	function searchItems() {
+		var searchParam = $('#search').val();
+		if(searchParam) {
+			window.location.href = '/search?search-param=' + searchParam;
+		} 
+	}
+});
