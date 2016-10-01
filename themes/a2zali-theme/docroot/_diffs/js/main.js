f1027b2c94c048c2ed23fd7b9f82492860bf83ba
Saleem Khan <786khan1saleem@gmail.com>
2016-10-01 22:35:26 +0530

    updated code with new script entries and js formating

--- themes/a2zali-theme/docroot/_diffs/js/main.js
+++ themes/a2zali-theme/docroot/_diffs/js/main.js
@@ -21,24 +21,23 @@ AUI().ready(
 	}
 );
 
-$(function(){
-	
+$(function() {
+
 	$(".quick-contact .control-group").removeClass("lfr-textarea-container");
-	$(".quick-contact .control-group").addClass( "form-inline" );
-	
+	$(".quick-contact .control-group").addClass("form-inline");
 
 	var elTop = $('#search-div').offset().top;
 	var mobile = !(Liferay.Util.isPhone() || Liferay.Util.isTablet());
-		$(window).scroll(function() {
-			if(mobile) {
-	        $('#search-div').toggleClass('sticky', $(window).scrollTop() > elTop);
-	        $('#fixed_div').toggleClass('fixed_div', $(window).scrollTop() > elTop);
+	$(window).scroll(function() {
+		if (mobile) {
+			$('#search-div').toggleClass('sticky', $(window).scrollTop() > elTop);
+			$('#fixed_div').toggleClass('fixed_div', $(window).scrollTop() > elTop);
 		} else {
 			$('#srch-btn').html('<i class="fa fa-search"></i>');
 		}
 	});
-	
-	$(document).on( 'scroll', function(){
+
+	$(document).on('scroll', function() {
 		if ($(window).scrollTop() > 100) {
 			$('#to_top a').removeClass('disabled');
 		} else {
@@ -46,91 +45,83 @@ $(function(){
 		}
 	});
 	$('#to_top').on('click', scrollToTop);
-	
+
 	// auto complete code
 	AUI().use('aui-base', function(A) {
-		Liferay.Service('/htmsd-portlet.shoppingitem/get-autocomplete-items',
-	  		function(data) {
-	  			var autocompleteData = data.autoCompleteData;
-				$("#search").autocomplete({
-					source : autocompleteData
-				});
-	  		}
-	  	);
+		Liferay.Service('/htmsd-portlet.shoppingitem/get-autocomplete-items', function(data) {
+			var autocompleteData = data.autoCompleteData;
+			$("#search_query_top").autocomplete({
+				source : autocompleteData
+			});
+		});
 	});
-	
+
 	function scrollToTop() {
-		verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+		verticalOffset = typeof (verticalOffset) != 'undefined' ? verticalOffset : 0;
 		element = $('body');
 		offset = element.offset();
 		offsetTop = offset.top;
-		$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+		$('html, body').animate({
+			scrollTop : offsetTop
+		}, 500, 'linear');
 	}
 
 	AUI().use('aui-base', function(A) {
 		Liferay.Service('/htmsd-portlet.shoppingcart/get-shopping-cart-item-count', {
-			userId: Liferay.ThemeDisplay.getUserId()
-		}, function(obj) {
-			console.info("Item COunt");
+			userId : Liferay.ThemeDisplay.getUserId()
+		},
+		function(obj) {
 			try {
 				if (parseInt(obj) > 0) {
 					$('.ajax_cart_quantity').show();
-					$('.ajax_cart_quantity').html("&nbsp;"+obj+"&nbsp;");
+					$('.ajax_cart_quantity').html("&nbsp;" + obj+ "&nbsp;");
 				}
-			} catch(err) {
+			} catch (err) {
 				console.error(err);
 			}
 		});
 	});
 
-	function searchItems() {
-		var searchParam = $('#search').val();
-		if(searchParam) {
+	$('#submit_searchbox', function() {
+		var searchParam = $('#search_query_top').val();
+		if (searchParam) {
 			window.location.href = '/search?search-param=' + searchParam;
-		} 
-	}
+		}
+	});
+	
 	var menuLeft = $('.pushmenu-left');
 	var nav_list = $('#nav_list');
 	var close = $('#pushMenuclose');
-	  
+
 	nav_list.click(function() {
 		$(this).toggleClass('active');
-	$('.pushmenu-push').toggleClass('pushmenu-push-toright');
-	menuLeft.toggleClass('pushmenu-open');
-	  });
-	  
-	close.click(function() {
 		$('.pushmenu-push').toggleClass('pushmenu-push-toright');
 		menuLeft.toggleClass('pushmenu-open');
 	});
 
-});
-//Multi-Toggle Navigation
-$(function() {
+	close.click(function() {
+		$('.pushmenu-push').toggleClass('pushmenu-push-toright');
+		menuLeft.toggleClass('pushmenu-open');
+	});
+	
+	// Multi-Toggle Navigation
 	$('body').addClass('js');
-		var	$menu = $('#menu'),
-			$menuTrigger = $('.has-subnav');
-
+	var $menu = $('#menu'), $menuTrigger = $('.has-subnav');
 	$menuTrigger.click(function(e) {
 		e.preventDefault();
 		var $this = $(this);
 		$this.toggleClass('active').next('ul').toggleClass('active');
 	});
-	
 	$('.has-subnav2').click(function(e) {
 		e.preventDefault();
 		var $this = $(this);
 		$this.toggleClass('active').next('ul').toggleClass('active');
 	});
-
-});
-
-// Remove "Active" Class from Menu on Resize
-$(window).resize(function() {
-	var viewportWidth = $(window).width();
+	
+	$(window).resize(function() {
+		var viewportWidth = $(window).width();
 		if (viewportWidth > 925) {
 			$("#menu").removeClass("active");
 		}
+	});
 });
-
-		
