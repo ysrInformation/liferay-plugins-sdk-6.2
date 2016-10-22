50605571209404d845366fbe279c37cce04d2a11
Saleem Khan <786khan1saleem@gmail.com>
2016-10-22 23:11:16 +0530

    added new entities, portlets(pickup, pricing)

---
+++ portlets/htmsd-portlet/docroot/WEB-INF/src/com/htmsd/pickup/PickUpPortlet.java
@@ -0,0 +1,11 @@
+package com.htmsd.pickup;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class PickUpPortlet
+ */
+public class PickUpPortlet extends MVCPortlet {
+ 
+
+}
---
+++ portlets/htmsd-portlet/docroot/WEB-INF/src/com/htmsd/pricing/PricingPortlet.java
@@ -0,0 +1,11 @@
+package com.htmsd.pricing;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class PricingPortlet
+ */
+public class PricingPortlet extends MVCPortlet {
+ 
+
+}
