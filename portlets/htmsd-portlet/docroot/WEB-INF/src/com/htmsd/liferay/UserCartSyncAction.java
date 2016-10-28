0afa0038244e3412699fb739b112843c4ecb5d08
riyaz <riyaz.pasha66@yahoo.com>
2016-10-29 00:01:10 +0530

    Updated addItemsToCart api

--- portlets/htmsd-portlet/docroot/WEB-INF/src/com/htmsd/liferay/UserCartSyncAction.java
+++ portlets/htmsd-portlet/docroot/WEB-INF/src/com/htmsd/liferay/UserCartSyncAction.java
@@ -6,9 +6,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.htmsd.slayer.NoSuchShoppingItem_CartException;
 import com.htmsd.slayer.model.ShoppingCart;
+import com.htmsd.slayer.model.ShoppingItem_Cart;
+import com.htmsd.slayer.model.WholeSale;
 import com.htmsd.slayer.service.ShoppingCartLocalServiceUtil;
 import com.htmsd.slayer.service.ShoppingItem_CartLocalServiceUtil;
+import com.htmsd.slayer.service.WholeSaleLocalServiceUtil;
 import com.htmsd.util.CommonUtil;
 import com.htmsd.util.ShoppingBean;
 import com.liferay.portal.kernel.events.Action;
@@ -79,12 +83,47 @@ public class UserCartSyncAction extends Action {
 		
 		if (Validator.isNotNull(shoppingBeanList) && shoppingBeanList.size() > 0) {
 			for (ShoppingBean shoppingBean:shoppingBeanList) {
-				 ShoppingItem_CartLocalServiceUtil.insertItemsToCart(shoppingBean.getQuantity(), 
-						 cartId, shoppingBean.getItemId(), shoppingBean.getTotalPrice());
+				ShoppingItem_Cart duplicateItem = null;
+				try {
+					duplicateItem = ShoppingItem_CartLocalServiceUtil.findByCartAndItemId(cartId, shoppingBean.getItemId());
+				} catch (NoSuchShoppingItem_CartException e) {
+					_log.info("No item exist with ItemID ->"+shoppingBean.getItemId()+" and  cartID ->"+cartId + "!, "+e.getMessage());  
+				} catch (SystemException e) {
+					_log.error("System error occurred due to :"+e);
+				}
+				
+				if (Validator.isNotNull(duplicateItem)) {
+					int qty = shoppingBean.getQuantity() + duplicateItem.getQuantity(); 
+					double totalPrice = getWholeSalePrice(qty, duplicateItem.getItemId(), shoppingBean.getUnitPrice());
+					ShoppingItem_CartLocalServiceUtil.updateShoppingItem_Cart(duplicateItem.getId(), qty, totalPrice);
+				} else {
+					ShoppingItem_CartLocalServiceUtil.insertItemsToCart(shoppingBean.getQuantity(), 
+							 cartId, shoppingBean.getItemId(), shoppingBean.getTotalPrice());	
+				}
 			}
 			_log.info("The items were added to DB successfully!"); 
 			session.removeAttribute("SHOPPING_ITEMS"); 
 		}
+	}
+	
+	private double getWholeSalePrice(int quantity, long itemId, double itemPrice) {
+		
+		double totalPrice = 0.0;
+		List<WholeSale> wholesaleList = WholeSaleLocalServiceUtil.getWholeSaleByQty(itemId, quantity);
+		if (Validator.isNotNull(wholesaleList)) {
+			totalPrice = quantity * itemPrice;
+			for (WholeSale wholeSale: wholesaleList) {
+				if (quantity == wholeSale.getQuantity()) {
+					totalPrice = wholeSale.getPrice(); 
+				} else if (quantity > wholeSale.getQuantity()) {
+					totalPrice = quantity * (wholeSale.getPrice() / wholeSale.getQuantity());
+				}
+			}
+		} else {
+			totalPrice = quantity * itemPrice;
+		}
+		
+		return totalPrice;
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(UserCartSyncAction.class);
