8b25deb7a476426a6f5425f99346588d993c7882
riyaz <riyaz.pasha66@yahoo.com>
2016-10-28 22:32:47 +0530

    Guest cart items after login fix

---
+++ portlets/htmsd-portlet/docroot/WEB-INF/src/com/htmsd/liferay/UserCartSyncAction.java
@@ -0,0 +1,92 @@
+package com.htmsd.liferay;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.htmsd.slayer.model.ShoppingCart;
+import com.htmsd.slayer.service.ShoppingCartLocalServiceUtil;
+import com.htmsd.slayer.service.ShoppingItem_CartLocalServiceUtil;
+import com.htmsd.util.CommonUtil;
+import com.htmsd.util.ShoppingBean;
+import com.liferay.portal.kernel.events.Action;
+import com.liferay.portal.kernel.events.ActionException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.User;
+import com.liferay.portal.util.PortalUtil;
+
+public class UserCartSyncAction extends Action {
+	/* (non-Java-doc)
+	 * @see com.liferay.portal.kernel.events.Action#Action()
+	 */
+	public UserCartSyncAction() {
+		super();
+	}
+
+	/* (non-Java-doc)
+	 * @see com.liferay.portal.kernel.events.Action#run(HttpServletRequest arg0, HttpServletResponse arg1)
+	 */
+	public void run(HttpServletRequest request, HttpServletResponse response) throws ActionException {
+		_log.info("In Login Post Action class (run method) @!!!"); 
+		addItemsToCart(request);
+	}
+	
+	protected void addItemsToCart(HttpServletRequest request) {
+		
+		HttpSession session = request.getSession();
+		
+		long groupId = 0l, cartId = 0l;
+		long userId = PortalUtil.getUserId(request);
+		long companyId = PortalUtil.getCompanyId(request);
+		
+		String fullName = StringPool.BLANK;
+		List<ShoppingBean> shoppingBeanList = CommonUtil.getGuestUserList(session);
+		try {
+			groupId = PortalUtil.getScopeGroupId(request);
+			User user = PortalUtil.getUser(request);
+			fullName = (Validator.isNotNull(user)) ? user.getFullName() : StringPool.BLANK;
+		} catch (PortalException e) {
+			_log.error("Portal exception occured due to :"+e); 
+		} catch (SystemException e) {
+			_log.error("System exception occured due to :"+e); 
+		}
+		
+		_log.info("The user ("+fullName+") has "+shoppingBeanList.size()+ " items in the cart!");
+		
+		ShoppingCart shoppingCart = null;
+		try {
+			shoppingCart = ShoppingCartLocalServiceUtil.getShoppingCartByUserId(userId);
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+		
+		if (Validator.isNotNull(shoppingCart)) {
+			cartId = shoppingCart.getCartId();
+		} else {
+			shoppingCart = ShoppingCartLocalServiceUtil.insertShoppingCart(
+					userId, companyId, groupId, fullName);
+			cartId = shoppingCart.getCartId();
+		}
+		
+		_log.info("CartId of "+ fullName +" is found to be "+cartId+".");  
+		
+		if (Validator.isNotNull(shoppingBeanList) && shoppingBeanList.size() > 0) {
+			for (ShoppingBean shoppingBean:shoppingBeanList) {
+				 ShoppingItem_CartLocalServiceUtil.insertItemsToCart(shoppingBean.getQuantity(), 
+						 cartId, shoppingBean.getItemId(), shoppingBean.getTotalPrice());
+			}
+			_log.info("The items were added to DB successfully!"); 
+			session.removeAttribute("SHOPPING_ITEMS"); 
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(UserCartSyncAction.class);
+
+}
